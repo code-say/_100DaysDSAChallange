3cf62aab8f2953fa0caf50a0e5637b7862ae1ee3
code-say <sayanmandal8643@gmail.com>
2023-10-02 22:53:25 +0530

    100 Days of DSA Challenge

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,31 @@
+package Sorting;
+import java.util.*;
+public class InsertionSort {
+    public static void insertionSortSln(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev =i-1;
+            //finding out the correct pos to insert
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            //insetion
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[] = {5,4,1,3,2};
+        //insertionSortSln(arr);
+        Arrays.sort(arr); // 0(n logn)
+        printArr(arr);
+    }
+}
